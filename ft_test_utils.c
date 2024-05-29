d13124125915dfef5e862d4094127b452034478b
David <dalabrad@student.42urduliz.com>
2024-05-29 18:33:56 +0200

    modified ft_test_utils.c

--- ft_test_utils.c
+++ ft_test_utils.c
@@ -6,17 +6,20 @@
 /*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 13:02:11 by dalabrad          #+#    #+#             */
-/*   Updated: 2024/05/24 15:21:53 by dalabrad         ###   ########.fr       */
+/*   Updated: 2024/05/29 18:28:31 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
+/*
+ * Prints a stack:
+ * n[0] = value_1 -> ... -> End of the Stack. 
+*/
 void	ft_print_stack(t_stack_node	*node)
 {
 	unsigned int	i;
 
-	i = 1;
+	i = 0;
 	if (!node)
 	{
 		ft_printf("Empty stack\n");
@@ -31,6 +34,10 @@ void	ft_print_stack(t_stack_node	*node)
 	ft_printf("End of Stack.\n");
 }
 
+/*
+ * Prints a Stack in reverse:
+ * End of the Stack. <- ... <- n[0] = value_1
+*/
 void	ft_print_stack_reverse(t_stack_node	*node)
 {
 	unsigned int	size;
@@ -48,7 +55,7 @@ void	ft_print_stack_reverse(t_stack_node	*node)
 	ft_printf("End of Stack.");
 	while (node)
 	{
-		ft_printf(" <-- n[%d] = %d", size, node->value);
+		ft_printf(" <-- n[%d] = %d", size - 1, node->value);
 		node = node->prev;
 		size--;
 	}
